64728fa8a765d1698de9974d5a6ecffcf6e6b0a2
Evgeniya K <ekwlshn@gmail.com>
2022-02-13 20:19:04 +0300

    feat: dont work dropdown :| go to hell bum

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,24 @@
-let menuItem = document.querySelector('.menu__item')
-let menuElem = document.querySelector('.leftbar-menu-bottom');
+let menuItem = document.querySelectorAll('.menu__item')
+let menuElem = document.querySelectorAll('.leftbar-menu-bottom');
 
+// menuItem.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   console.log('im work');
+//   menuItem.classList.toggle('open');
+//   menuElem.classList.toggle('open');
+// })
 
-menuItem.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log('im work');
-  menuItem.classList.toggle('open');
-  menuElem.classList.toggle('open');
+menuItem.forEach(function (element){
+  element.onclick = function () {
+    menuItem.classList.add('open');
+    menuElem.classList.add('open');
+    menuElem.forEach(function (element){
+      console.log(element)
+    })
+  }
+
 })
+
 
 
 
