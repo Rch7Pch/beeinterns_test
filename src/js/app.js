3085b5cd16f1883013c891e572d48e1461322a1d
Evgeniya K <ekwlshn@gmail.com>
2022-02-14 14:56:45 +0300

    feat: its working!!!!!!!!

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,29 @@
-let menuItem = document.querySelectorAll('.menu__item')
-let menuElem = document.querySelectorAll('.leftbar-menu-bottom');
+let menuItem = document.querySelector('.menu__item')
+let menuElem = document.querySelector('.leftbar-menu-bottom');
 
-// menuItem.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   console.log('im work');
-//   menuItem.classList.toggle('open');
-//   menuElem.classList.toggle('open');
-// })
-
-menuItem.forEach(function (element){
-  element.onclick = function () {
-    menuItem.classList.add('open');
-    menuElem.classList.add('open');
-    menuElem.forEach(function (element){
-      console.log(element)
-    })
-  }
-
+menuItem.addEventListener('click', (e) => {
+  e.preventDefault();
+  // console.log('im work');
+  // menuItem.classList.toggle('open');
+  // menuElem.classList.toggle('open');
 })
+
+const onClickTitle = (e) => {
+  console.log(e.target.parentNode);
+  e.target.parentNode.classList.toggle('open');
+}
+
+
+// menuItem.forEach(function (element){
+//   element.onclick = function () {
+//     menuItem.classList.add('open');
+//     menuElem.classList.add('open');
+//     menuElem.forEach(function (element){
+//       console.log(element)
+//     })
+//   }
+//
+// })
 
 
 
